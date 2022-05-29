479be957a9a75cf082fb4f4b574bae83dab087f7
swati <91478967+swati-kaithwas@users.noreply.github.com>
2022-05-30 01:45:33 +0530

    s

--- src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-const connect = require("./configs/db");
-const port = process.env.PORT || 2566;
-app.listen(port, async function () {
-  await connect();
-  console.log("listening on port 2566:", +port);
-});
